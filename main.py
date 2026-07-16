e142b5599572f195a00fa60fb01e343d3d5e5d64
Vitor Nunes <vitornunees38@gmail.com>
2026-07-15 22:04:54 -0300

    More results added

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ tolerancia = 1e-2
 #clusters_default = 16
 #clusters_input = input(f'Insira o número de cores (clusters) na imagem comprimida (default = {clusters_default}): ')
 #clusters = int(clusters_input) if clusters_input else clusters_default
-clusters_opt = [32, 64, 128]
+clusters_opt = [2, 4, 8, 16, 32, 64]
 
 
 for clusters in clusters_opt:
@@ -27,7 +27,7 @@ for clusters in clusters_opt:
   fim = time.time() #Finaliza tempo de execução do k-means
 
   print(f"\nGerando gráfico 3D para {clusters} cores...")
-#   plot3d(pontos, centroides, clusters)
+  plot3d(pontos, centroides, clusters)
 
   recuperado = comprime_imagem(centroides, indice, img, clusters)
   mse = calcular_mse(img, recuperado)
